39c58377c4ee40a5ac28664599c1eab54f39c1d9
Homygo25 <nagacclark@gmail.com>
2025-06-03 23:18:56 +0800

    updated currency converter api

--- resources/js/components/client/package/PaymentOnline.tsx
+++ resources/js/components/client/package/PaymentOnline.tsx
@@ -23,7 +23,7 @@ export function PaymentOnline({ open, finalValues, onOpen, receiving_bank }: Pac
     useEffect(() => {
         const fetchExchangeRate = async () => {
             try {
-                const response = await fetch('https://v6.exchangerate-api.com/v6/8fd94660d77127bfdfe04f5d/latest/USD');
+                const response = await fetch('https://v6.exchangerate-api.com/v6/00ffcb23a9f756e0e26d214e/latest/USD');
                 if (!response.ok) {
                     throw new Error('Network response was not ok');
                 }
