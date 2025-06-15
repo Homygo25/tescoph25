8ee7d84509c3da1956f9d82a61aa595421596510
Homygo25 <nagacclark@gmail.com>
2025-06-15 22:46:21 +0800

    Fix migrations and factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -29,6 +29,9 @@ class UserFactory extends Factory
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
             'remember_token' => Str::random(10),
+            'username' => fake()->unique()->userName(),
+            'gender' => fake()->randomElement(['m', 'f', 'o']),
+            'role' => 'client',
         ];
     }
 
