e645e7c1870f974bc98f92594f7856a02b81ec55
Homygo25 <nagacclark@gmail.com>
2025-06-24 01:38:29 +0900

    Update ReceivingBankSeeder.php

--- database/seeders/ReceivingBankSeeder.php
+++ database/seeders/ReceivingBankSeeder.php
@@ -15,24 +15,28 @@ class ReceivingBankSeeder extends Seeder
     {
         //
         ReceivingBank::updateOrInsert(
-            [
-                'bank_name' => 'BPI',
-                'receiving_bank' => 'Bank of the Philippine Islands/ BPI Savings',
-                'payment_channel' => 'Bank Transfer - Instapay',
-                'account_name' => 'Tesco Supermart Incoporation',
-                'account_number' => '4006890693 ',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
-            [
-                'bank_name' => 'RCBC',
-                'receiving_bank' => 'RCBC Savings',
-                'payment_channel' => 'Bank Transfer - Instapay',
-                'account_name' => 'Tesco Supermart Inc.',
-                'account_number' => '7591395917',
-                'created_at' => now(),
-                'updated_at' => now(),
-            ],
+    ['bank_name' => 'BPI'],
+    [
+        'receiving_bank' => 'Bank of the Philippine Islands/ BPI Savings',
+        'payment_channel' => 'Bank Transfer - Instapay',
+        'account_name' => 'Tesco Supermart Incorporation',
+        'account_number' => '4006890693',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]
+);
+
+ReceivingBank::updateOrInsert(
+    ['bank_name' => 'RCBC'],
+    [
+        'receiving_bank' => 'RCBC Savings',
+        'payment_channel' => 'Bank Transfer - Instapay',
+        'account_name' => 'Tesco Supermart Inc.',
+        'account_number' => '7591395917',
+        'created_at' => now(),
+        'updated_at' => now(),
+    ]
+);
             // [
             //     'bank_name' => 'GoTyme',
             //     'receiving_bank' => 'GoTyme Bank',
