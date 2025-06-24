1ba7652305a8328639d53284d0e447a7db9c42ff
Homygo25 <nagacclark@gmail.com>
2025-06-24 13:27:42 +0800

    Update ReceivingBankSeeder.php

--- database/seeders/ReceivingBankSeeder.php
+++ database/seeders/ReceivingBankSeeder.php
@@ -3,7 +3,6 @@
 namespace Database\Seeders;
 
 use App\Models\ReceivingBank;
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
 class ReceivingBankSeeder extends Seeder
@@ -13,39 +12,41 @@ class ReceivingBankSeeder extends Seeder
      */
     public function run(): void
     {
-        //
         ReceivingBank::updateOrInsert(
-    ['bank_name' => 'BPI'],
-    [
-        'receiving_bank' => 'Bank of the Philippine Islands/ BPI Savings',
-        'payment_channel' => 'Bank Transfer - Instapay',
-        'account_name' => 'Tesco Supermart Incorporation',
-        'account_number' => '4006890693',
-        'created_at' => now(),
-        'updated_at' => now(),
-    ]
-);
+            ['bank_name' => 'BPI'],
+            [
+                'receiving_bank' => 'Bank of the Philippine Islands/ BPI Savings',
+                'payment_channel' => 'Bank Transfer - Instapay',
+                'account_name' => 'Tesco Supermart Incorporation',
+                'account_number' => '4006890693',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
 
-ReceivingBank::updateOrInsert(
-    ['bank_name' => 'RCBC'],
-    [
-        'receiving_bank' => 'RCBC Savings',
-        'payment_channel' => 'Bank Transfer - Instapay',
-        'account_name' => 'Tesco Supermart Inc.',
-        'account_number' => '7591395917',
-        'created_at' => now(),
-        'updated_at' => now(),
-    ]
-);
-            // [
-            //     'bank_name' => 'GoTyme',
-            //     'receiving_bank' => 'GoTyme Bank',
-            //     'payment_channel' => 'Bank Transfer - Instapay',
-            //     'account_name' => 'Tesco Supermart Incoporation',
-            //     'account_number' => '017537760396',
-            //     'created_at' => now(),
-            //     'updated_at' => now(),
-            // ],
-        ]);
+        ReceivingBank::updateOrInsert(
+            ['bank_name' => 'RCBC'],
+            [
+                'receiving_bank' => 'RCBC Savings',
+                'payment_channel' => 'Bank Transfer - Instapay',
+                'account_name' => 'Tesco Supermart Inc.',
+                'account_number' => '7591395917',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ]
+        );
+
+        // Uncomment to add GoTyme
+        // ReceivingBank::updateOrInsert(
+        //     ['bank_name' => 'GoTyme'],
+        //     [
+        //         'receiving_bank' => 'GoTyme Bank',
+        //         'payment_channel' => 'Bank Transfer - Instapay',
+        //         'account_name' => 'Tesco Supermart Incorporation',
+        //         'account_number' => '017537760396',
+        //         'created_at' => now(),
+        //         'updated_at' => now(),
+        //     ]
+        // );
     }
 }
